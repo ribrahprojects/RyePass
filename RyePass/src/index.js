61898747d92a84eb5f2c527bc616ea79b16d69ba
ribrah <ribrahprojects@gmail.com>
2021-02-18 20:38:42 -0500

    able to use text and button to show single pasword on screen

--- RyePass/src/index.js
+++ RyePass/src/index.js
@@ -9,8 +9,8 @@ if (require('electron-squirrel-startup')) { // eslint-disable-line global-requir
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1000,
+    height: 800,
   });
 
   // and load the index.html of the app.
@@ -44,3 +44,16 @@ app.on('activate', () => {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
+
+
+function passwordEntered(){
+  var input = document.getElementById("Pass").value;
+  //alert(input);
+  //updatePasswords(input);
+  document.getElementById("passwordList").innerHTML = input;
+
+}
+
+function updatePasswords(input){
+  document.getElementById("passwordList").innerHTML = input;
+}
